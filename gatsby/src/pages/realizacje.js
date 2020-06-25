194485ea384a0082815a94e907cd4fd5d129e7f0
Kozakiewicz <emir.alobedi@gmail.com>
2020-06-25 21:39:48 +0200

    podstrona realizacje zrobione query

--- gatsby/src/pages/realizacje.js
+++ gatsby/src/pages/realizacje.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from "react"
-import { Link } from "gatsby"
-import "../style/cennik.scss"
+import "../style/index.scss"
+import { graphql } from 'gatsby'
+import Img from "gatsby-image"
+import Layout from "../components/layout"
 
 import SEO from "../components/seo"
 
@@ -8,11 +10,10 @@ import Counter from "../components/counter"
 import Faq from "../components/faq"
 import Cta from "../components/cta"
 import { motion } from "framer-motion"
-import Layout from "../components/layout"
-import "../style/index.scss"
 import gsap, { Power3 } from "gsap"
+import { v4 as uuidv4 } from 'uuid';
 
-const Realizacje = () => {
+const Realizacje = ({data}) => {
   const ease = Power3.easeOut
   useEffect(() => {
     gsap.fromTo(
@@ -30,95 +31,24 @@ const Realizacje = () => {
         ease: ease,
       }
     )
-    gsap.fromTo(
-      ".price-item",
-      {
-        y: 50,
-        opacity: 0,
-      },
-      {
-        duration: 0.6,
-        y: 0,
-        opacity: 1,
-        delay: 0.3,
-        stagger: 0.1,
-        ease: ease,
-      }
-    )
+  
   }, [])
-
-  const data = [
-    {
-      title: "Ulotka 1-stronna",
-      price: "90 zł netto",
-      content: "Jednostronna ulotka o dowolnym wymiarze",
-    },
-    {
-      title: "Ulotka 2-stronna",
-      price: "135 zł netto",
-      content: "Dwustronna ulotka o dowolnym wymiarze",
-    },
-    {
-      title: "Wizytówka",
-      price: "65 zł netto",
-      content:
-        "Dwustronna wizytówka o dowolnym wymiarze. Najczęściej 90x55mm albo 85x50mm",
-    },
-    {
-      title: "Plakat",
-      price: "120 zł netto",
-      content: "Projekt plakatu o dowolnych wymiarach",
-    },
-    {
-      title: "Projekt opakowania",
-      price: "Wycena indywidualna",
-      content: "Projekt graficzny dowolnego opakowania",
-    },
-    {
-      title: "Dowolny projekt graficzny",
-      price: "125 zł netto",
-      content: "Projekt graficzny o dowolnych wymiarach",
-    },
-    {
-      title: "Baner na facebooka",
-      price: "60 zł netto",
-      content:
-        "Projekt graficzny baneru na Facebooka (zdjęcie w tle) o wymiarach dopasowanych do obecnych wymogów Facebooka",
-    },
-    {
-      title: "Favicon",
-      price: "19 zł netto",
-      content: "Miniatura w karcie na pasku przeglądarki",
-    },
-    {
-      title: "Projekt stopki e-mail",
-      price: "85 zł netto",
-      content: "Projekt graficzny stopki e-mail w HTML",
-    },
-    {
-      title: "Księga znaku",
-      price: "499 zł netto",
-      content: "Zaawansowana i rozbudowana księga znaku dla Twojej marki",
-    },
-    {
-      title: "Strona internetowa",
-      price: "Indywidualna wycena",
-      content:
-        "Zbudujemy dla Ciebie responsywną i zoptymalizowaną stronę internetową z najnowszych technologiach",
-    },
-    {
-      title: "Sklep internetowy",
-      price: "Indywidualna wycena",
-      content:
-        "Zbudujemy dla Ciebie szybki i gotowy do sprzedaży sklep w najnowszych technologiach e-commerce",
-    },
-  ]
+console.log(data.allFile.edges)
   return (
     <>
       <Layout>
         <SEO title="Page two" />
         <section className="container flex flex-col items-center mx-auto py-4">
           <h1>Realizacje</h1>
+           <div className="w-full flex flex-row flex-wrap">
+           {data.allFile.edges.map(logo => (
+                <div key={uuidv4()} className="w-1/6">
+                  <Img
+                className="w-full"
+                fluid={logo.node.childImageSharp.fluid}
+              /></div>
+            ))}
+           </div>
         </section>
         <Cta />
         <Faq />
@@ -129,4 +59,20 @@ const Realizacje = () => {
   )
 }
 
+export const query = graphql`
+  query logosy {
+    allFile(filter: { relativeDirectory: { eq: "logasy" } }) {
+            edges {
+              node {
+                id
+                childImageSharp {
+                  fluid {
+                    ...GatsbyImageSharpFluid
+                  }
+                }
+              }
+            }
+          }
+  }
+`
 export default Realizacje
