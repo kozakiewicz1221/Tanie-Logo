de804a75c238ebdec11d5c65a75642bfcc2734b4
Kozakiewicz <emir.alobedi@gmail.com>
2020-06-29 19:57:27 +0200

    added home GET

--- app.js
+++ app.js
@@ -19,7 +19,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.set("view engine", "jade");
 
 app.use("/", indexRouter);
 
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,9 @@ var router = express.Router();
 const neworder = require("../controllers/neworder");
 const admin = require("../controllers/admin");
 
+router.get("/", (req, res) => {
+  res.send("okok");
+});
 router.post("/api/order", neworder.neworder);
 router.get("/api/deleteorders", neworder.deleteorders);
 router.get("/api/admin", admin.admin);
